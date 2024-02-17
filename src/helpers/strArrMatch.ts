48ed89e73f0ee97449bafcf081266697672fe027
Haobo Zhang <haobozhang9081@gmail.com>
2024-02-17 15:45:54 -0800

    fix optimize

--- src/helpers/strArrMatch.ts
+++ src/helpers/strArrMatch.ts
@@ -1,9 +1,10 @@
+import arrDeepIndex from './arrDeepIndex';
 import makeArray from './makeArray';
 
 export default function strArrMatch(arr: string | string[], str: string, exact?: boolean) {
 	if (arr === undefined) return true;
 	arr = makeArray(arr);
 	const index = arr.indexOf('critRD_');
-	if (index !== -1) arr = arr.toSpliced(index, 1, 'critRate_', 'critDMG_');
-	return exact ? arr.indexOf(str) === 0 : arr.indexOf(str) !== -1;
+	if (index !== -1) arr[index] = ['critRate_', 'critDMG_'] as any;
+	return exact ? arrDeepIndex(arr, str) === 0 : arrDeepIndex(arr, str) !== -1;
 }
