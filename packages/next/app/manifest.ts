30ac79a2dec2cc365ede66584c662932fc51ee4e
Haobo Zhang <haobozhang9081@gmail.com>
2025-03-18 09:54:31 -0700

    updated icons

--- packages/next/app/manifest.ts
+++ packages/next/app/manifest.ts
@@ -12,5 +12,19 @@ export default function manifest(): MetadataRoute.Manifest {
 		orientation: 'portrait',
 		theme_color: '#ffffff',
 		background_color: '#ffffff',
+		icons: [
+			{
+				src: '/web-app-manifest-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+				purpose: 'maskable',
+			},
+			{
+				src: '/web-app-manifest-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'maskable',
+			},
+		],
 	};
 }
