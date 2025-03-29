4cbb32e56b88ff6cc7e470ebe73d21f0b35e8d04
Haobo Zhang <haobozhang9081@gmail.com>
2025-03-29 13:34:03 -0700

    show role on weapons edit

--- packages/next/app/characters/[name]/characterWeaponTier.tsx
+++ packages/next/app/characters/[name]/characterWeaponTier.tsx
@@ -1,18 +1,21 @@
 import makeArray from '@/src/helpers/makeArray';
 import type { Build } from '@/src/types/data';
-import { AvatarGroup, Stack } from '@mui/material';
+import { AvatarGroup, Box, Stack, Typography } from '@mui/material';
 import WeaponImage from '../../weapons/weaponImage';
 
 export default function CharacterWeaponTier({ build }: { build: Build }) {
 	return (
-		<Stack direction='row' spacing={1} sx={{ flexWrap: 'wrap' }}>
-			{build.weapon.map((weaponTier, index) => (
-				<AvatarGroup key={index} sx={{ flexDirection: 'row-reverse' }} variant='rounded'>
-					{makeArray(weaponTier).map((weapon) => (
-						<WeaponImage key={weapon} weapon={weapon} size={50} />
-					))}
-				</AvatarGroup>
-			))}
-		</Stack>
+		<Box>
+			<Typography sx={{ my: 0.5 }}>{build.role}</Typography>
+			<Stack direction='row' spacing={1} sx={{ flexWrap: 'wrap' }}>
+				{build.weapon.map((weaponTier, index) => (
+					<AvatarGroup key={index} sx={{ flexDirection: 'row-reverse' }} variant='rounded'>
+						{makeArray(weaponTier).map((weapon) => (
+							<WeaponImage key={weapon} weapon={weapon} size={50} />
+						))}
+					</AvatarGroup>
+				))}
+			</Stack>
+		</Box>
 	);
 }
