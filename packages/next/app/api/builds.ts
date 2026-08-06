e928b83f771725ae4b29e03856203572131be131
Haobo Zhang <haobozhang9081@gmail.com>
2026-08-05 22:01:25 -0700

    fix

--- packages/next/app/api/builds.ts
+++ packages/next/app/api/builds.ts
@@ -1,7 +1,8 @@
 import { type Build } from '@/types/data';
 import { type CharacterKey } from '@/types/good';
 import data from '@/public/data/builds.json';
+import { omit } from 'remeda';
 
-export const builds: Record<CharacterKey, Build | Build[]> = data as any;
+export const builds: Record<CharacterKey, Build | Build[]> = omit(data, ['@lastUpdated']) as any;
 
 export const buildsList = Object.values(builds).flat();
